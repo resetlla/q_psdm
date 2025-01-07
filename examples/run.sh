d790705b4ea442178a2763edcad11818823aebcc
resetlla <hsinav.v@gmail.com>
2025-01-07 16:35:43 +0800

    add run.sh

--- examples/run.sh
+++ examples/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+cat ./traveltime/L350.ttt_* > ./traveltime/L350.ttt
+cat ./vel/vel_loadin_* > ./vel_vel_loadin
+
 ../bin/qmig3dd_multithreading path=./0/qmig3dd parfilepath=./0/qmig3dd/par/par.dat ttpath=./traveltime nodename=node2_0
