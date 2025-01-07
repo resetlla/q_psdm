aa76eddb139b77eef22b796d0705d609b93105fb
resetlla <hsinav.v@gmail.com>
2025-01-07 16:53:27 +0800

    fix run.sh

--- examples/run.sh
+++ examples/run.sh
@@ -3,4 +3,4 @@
 cat ./traveltime/L350.ttt_* > ./traveltime/L350.ttt
 cat ./vel/vel_loadin_* > ./vel_vel_loadin
 
-../bin/qmig3dd_multithreading path=./0/qmig3dd parfilepath=./0/qmig3dd/par/par.dat ttpath=./traveltime nodename=node2_0
+../src/qmig3dd_multithreading path=./0/qmig3dd parfilepath=./0/qmig3dd/par/par.dat ttpath=./traveltime nodename=node2_0
